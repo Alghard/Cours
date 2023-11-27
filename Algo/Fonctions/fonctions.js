98d5c9c31175df45774564f1a4c8ea93f0489866
Alghard <julien.varela@gmail.com>
2023-11-27 17:00:17 +0100

    maj cours algo

--- Algo/Fonctions/fonctions.js
+++ Algo/Fonctions/fonctions.js
@@ -1,22 +1,45 @@
 // Écrire une fonction f1 qui ne prend rien en entrée, et qui affiche 17 dans la console.
-
+function f1(){
+    console.log("17");
+}
 // Utiliser la fonction f1 pour afficher 17 dans la console.
+f1();
 
 // Écrire une fonction f2 qui ne prend rien en entrée, et qui retourne 17.
+function f2(){
+    let result = 17;
+    return result;
+}
 
 // Utiliser la fonction f2 pour afficher 17 dans la console.
+console.log(f2());
 
 // La fonction f2 a l'air moins pratique à utiliser. Quel intérêt pourrait-elle avoir par rapport à f1 ?
 
+//On peut utiliser le résultat
+
 // Écrire une fonction f3 qui prend un nombre en entrée et qui affiche dans la console le double de ce nombre.
+function f3(n){
+    let result;
+    result = n*2;
+    console.log(result);
+}
 
 // Utiliser la fonction f3 pour écrire dans la console le double de 99.
+f3(99);
 
 // Écrire une fonction f4 qui prend un nombre en entrée et qui retourne le double de ce nombre.
+function f4(n){
+    let result;
+    result = n*2;
+    return result;
+}
 
 // Utiliser la fonction f4 pour écrire dans la console le double de 99.
+console.log(f4(99));
 
 // Utiliser la fonction f4 pour stocker dans une variable appelée a le double de 99.
+let result = f4(99);
 
 // Écrire une fonction f5 qui prend en entrée deux nombres et qui affiche dans la console la somme de ces deux nombres.
 
