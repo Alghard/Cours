2e2517aac924e91968e3c238baabda20ab5a342f
Alghard <julien.varela@gmail.com>
2023-11-28 10:47:07 +0100

    Exo algo finis

--- Algo/Fonctions/fonctions.js
+++ Algo/Fonctions/fonctions.js
@@ -39,21 +39,76 @@ function f4(n){
 console.log(f4(99));
 
 // Utiliser la fonction f4 pour stocker dans une variable appelée a le double de 99.
-let result = f4(99);
+let a = f4(99);
 
 // Écrire une fonction f5 qui prend en entrée deux nombres et qui affiche dans la console la somme de ces deux nombres.
+function f5(x,y){
+    let result = x + y;
+    console.log(result);
+}
 
 // Utiliser la fonction f5 pour écrire dans la console la somme de 42 et 77.
+f5(42,77);
 
 // Écrire une fonction f6 qui prend en entrée deux nombres et qui retourne la somme de ces deux nombres.
+function f6(x,y){
+    let result = x + y;
+    return result;
+}
 
 // Utiliser la fonction f6 pour écrire dans la console la somme de 42 et 77.
+console.log(f6(42,77));
 
 // Utiliser la fonction f6 pour incrémenter la variable a de la somme de 42 et 77.
+a += f6(42,77);
+console.log(a);
 
 // Écrire une fonction f7 qui prend deux nombres en entrée et qui retourne le plus grand des deux.
-
+function f7(x,y){
+    if (x > y){
+        return x;
+    }
+    else {
+        return y;
+    }
+}
 // Écrire une fonction f8 qui prend trois nombres en entrée et qui retourne le plus grand des trois.
 // (Au moins deux versions possibles : sans utiliser f7, et en utilisant f7).
+function f8a(x,y,z){
+    if (x > y && x > z){
+        return x;
+    }
+    else if (y > x && y > z){
+        return y;
+    }
+    else{
+        return z;
+    }
+}
+
+console.log(f8a(125,78,1110));
+
+function f8b(x,y,z){
+    a = f7(x,y);
+    if (a > z){
+        return a;
+    }
+    else {
+        return z;
+    }
+}
+console.log(f8b(546,778,1112));
 
 // Écrire une fonction f9 qui prend un nombre et un mot en entrée, et qui affiche dans la console ce mot ce nombre de fois.
+function f9(nb,txt){
+    let i = 0;
+    if (nb < i){
+        console.log("Erreur, veuillez rentrer un nombre > 0")
+    }
+    else {
+        for ( i ; i < nb ; i++){
+        console.log(txt);
+        }
+    }
+}
+f9(58,"RIP BOZO REST IN PISS");
